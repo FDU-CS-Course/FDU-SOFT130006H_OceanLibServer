7b3dd52faf3f4221f5c65fff53c3b4666a33f219
AInfinity-LilacDream <1977741520@qq.com>
2025-05-06 11:49:36 +0800

    fix: null safety check

--- ocean-collection-service/src/main/java/com/oriole/ocean/service/UserCollectionServiceImpl.java
+++ ocean-collection-service/src/main/java/com/oriole/ocean/service/UserCollectionServiceImpl.java
@@ -68,6 +68,10 @@ public class UserCollectionServiceImpl {
 
         AggregationResults<UserCollectionEntity.CollectionEntity> results = mongoTemplate.aggregate(aggregation,
                 getCollectionName(mainType), UserCollectionEntity.CollectionEntity.class);
+
+        if (results.getMappedResults() == null) {
+            return null;
+        }
         return results.getMappedResults().get(0);
     }
 
