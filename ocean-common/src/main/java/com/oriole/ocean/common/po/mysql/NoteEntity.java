a89376bfe7b74783d5cadada71fc3bc8fc416e41
AInfinity-LilacDream <1977741520@qq.com>
2025-05-17 21:32:47 +0800

    minor: update NoteEntity

--- ocean-common/src/main/java/com/oriole/ocean/common/po/mysql/NoteEntity.java
+++ ocean-common/src/main/java/com/oriole/ocean/common/po/mysql/NoteEntity.java
@@ -14,22 +14,13 @@ public class NoteEntity implements java.io.Serializable {
     private Integer noteID;
     private String tag;
     private String content;
-    @TableId("like_num")
     private Integer likeNum;
-    @TableId("comment_num")
     private Integer commentNum;
-    @TableId("read_num")
     private Integer readNum;
-    @TableId("refresh_date")
     private Date refreshDate;
-    @TableId("build_date")
     private Date buildDate;
-    @TableId("build_username")
     private String buildUsername;
-    @TableId("is_anon")
+    private boolean isDeleted;
     private Byte isAnon;
-    @TableId("is_approved")
-    private Byte isApproved;
-    @TableId("is_allow_comment")
     private Byte isAllowComment;
 }
