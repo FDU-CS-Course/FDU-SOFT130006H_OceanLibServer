4a7410480d905d82a462b9b97b1900ad0aee89c3
AInfinity-LilacDream <1977741520@qq.com>
2025-05-18 10:43:44 +0800

    fix: add translation between NoteEntity and note db

--- ocean-common/src/main/java/com/oriole/ocean/common/po/mysql/NoteEntity.java
+++ ocean-common/src/main/java/com/oriole/ocean/common/po/mysql/NoteEntity.java
@@ -1,5 +1,6 @@
 package com.oriole.ocean.common.po.mysql;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -10,21 +11,39 @@ import java.util.Date;
 @Data
 @TableName("`note`")
 public class NoteEntity implements java.io.Serializable {
-    @TableId("id")
-    private Integer id;
+    @TableId("note_id")
+    private String id;
+
+    @TableField("build_username")
     private String buildUsername;
-    // contents
+
+    @TableField("content")
     private String content;
+
+    @TableField("tag")
     private String tag;
-    // extra info
+
+    @TableField("build_date")
     private Date buildDate;
+
+    @TableField("refresh_date")
     private Date refreshDate;
-    // states
+
+    @TableField("is_deleted")
     private Byte isDeleted;
+
+    @TableField("is_anon")
     private Byte isAnon;
+
+    @TableField("is_allow_comment")
     private Byte isAllowComment;
-    // statistics (changing with relation 'likes')
+
+    @TableField("like_num")
     private Integer likeNum;
+
+    @TableField("comment_num")
     private Integer commentNum;
+
+    @TableField("read_num")
     private Integer readNum;
 }
